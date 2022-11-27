ae29891084236dfe56c52a4ceeea986818509ccf
FazalM <117861556+FazalM@users.noreply.github.com>
2022-11-27 15:38:36 +0000

    Add files via upload

---
+++ Casesar cypher.py
@@ -0,0 +1,20 @@
+word = input("Enter a word: ")
+key = int(input("enter the shift number: "))
+alphabet = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k","l", "m", "n", "o", "p","q", "r", "s", "t", "u","v", "w", "x", "y", "z", " "]
+newWord = ""
+
+def caesarCypher(word, key, newWord):
+
+    for x in word:
+        position = alphabet.index(x)
+        print(f"original position {position}")
+        position += key
+        print(f"new position {position}")
+        if position == len(alphabet):
+            position -= len(alphabet)
+        newWord += alphabet[position]
+    return newWord
+
+
+newWord = caesarCypher(word, key, newWord)
+print(f"encrypted word is: {newWord}") 
